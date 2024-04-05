0b878cab76f8363a4d8d6c4e8dc47f345d646e8a
ieliasz <ieliasz@NB_IELIASZ.comercio.local>
2024-04-05 10:24:30 -0500

    VErsionando los cambios para la nueva version

--- src/main/java/org/ea/VersioningDemo.java
+++ src/main/java/org/ea/VersioningDemo.java
@@ -8,5 +8,6 @@ public class VersioningDemo {
         System.out.println("Hello explorers.");
         System.out.println("  --------------------------------------------------------------------------------------------  ");
         System.out.println("This program is running version " + VersioningDemo.class.getPackage().getImplementationVersion());
+        System.out.println("  --------------------------------------------------------------------------------------------  ");
     }
 }
